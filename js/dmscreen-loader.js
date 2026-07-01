e42c1314f03597fb8d60446e25ea9eaa79a6bf63
RFuentes <rothaliph@gmail.com>
2026-06-30 22:25:54 -0400

    Fix deployed DM screen assets

--- js/dmscreen-loader.js
+++ js/dmscreen-loader.js
@@ -1,1 +1,8 @@
-window.addEventListener("load", () => import("./dmscreen.js"));
+const pLoadDmScreen = (() => {
+	let pLoad = null;
+
+	return () => pLoad ||= import("./dmscreen.js");
+})();
+
+if (document.readyState === "complete") pLoadDmScreen();
+else window.addEventListener("load", pLoadDmScreen, {once: true});
